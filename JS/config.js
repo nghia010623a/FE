ffc3dccd46116e4d2d4bc9a0a1cb90d0b6bf56cf
nghia010623a <duc91055@gmail.com>
2026-08-20 21:12:56 +0700

    aa

--- JS/config.js
+++ JS/config.js
@@ -1,3 +1,3 @@
-const API_BASE = "https://easily-significantly-sculpture-opposed.trycloudflare.com/";
+const API_BASE = "hhttps://cdt-aspects-tend-studios.trycloudflare.com/";
 
 // ma giam gia, payos,fix minus pd
