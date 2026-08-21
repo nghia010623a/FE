070a65d1330ca4bb980fc63138ba76e18dbdfd53
nghia010623a <duc91055@gmail.com>
2026-08-21 13:40:37 +0700

    aaa

--- JS/config.js
+++ JS/config.js
@@ -1,3 +1,3 @@
-const API_BASE = "https://banners-processed-prize-supplier.trycloudflare.com/";
+const API_BASE = "https://explains-kings-homeland-postal.trycloudflare.com/";
 
 // ma giam gia, payos,fix minus pd
